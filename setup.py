d8187647c7b707834a6eac3d5042244e90380d11
an66ezic <antony.aj.jose@fau.de>
2023-11-13 23:16:06 +0100

    modified setup file

--- setup.py
+++ setup.py
@@ -1,1 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your_project_name',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your project dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'your_script_name = your_module_name:main',
+        ],
+    },
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of your project',
+    url='https://github.com/your_username/your_project',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
 
